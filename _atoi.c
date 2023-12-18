ab122ff5ba5b355c27f3a429e86506c31a77b212
99122463500 <karabononyane99@gmail.com>
2023-12-17 23:54:39 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 
+// Function to calculate the factorial of a number
+unsigned long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
 int main() {
-    int num1, num2, sum;
+    int number;
 
-    // Prompt the user to enter the first number
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    // Prompt the user to enter a number
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &number);
 
-    // Prompt the user to enter the second number
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // Check if the number is non-negative
+    if (number < 0) {
+        printf("Please enter a non-negative integer.\n");
+        return 1; // Return an error code
+    }
 
-    // Calculate the sum
-    sum = num1 + num2;
-
-    // Display the result
-    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+    // Calculate and display the factorial
+    printf("The factorial of %d is: %llu\n", number, factorial(number));
 
     return 0;
 }
