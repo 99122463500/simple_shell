dcad75859a3cdd0945abe504d65cbf7a0f40db54
99122463500 <karabononyane99@gmail.com>
2023-12-17 23:58:08 -0800

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 
-// Function to calculate the factorial of a number
-unsigned long long factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
+/**
+ * calculate_factorial - Calculate the factorial of a non-negative integer.
+ * @n: The number for which factorial is calculated.
+ *
+ * Return: The factorial of n.
+ */
+unsigned long long calculate_factorial(int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+	else
+		return n * calculate_factorial(n - 1);
 }
 
-int main() {
-    int number;
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(void)
+{
+	int number;
 
-    // Prompt the user to enter a number
-    printf("Enter a non-negative integer: ");
-    scanf("%d", &number);
+	/* Prompt the user to enter a number */
+	printf("Enter a non-negative integer: ");
+	scanf("%d", &number);
 
-    // Check if the number is non-negative
-    if (number < 0) {
-        printf("Please enter a non-negative integer.\n");
-        return 1; // Return an error code
-    }
+	/* Check if the number is non-negative */
+	if (number < 0)
+	{
+		printf("Please enter a non-negative integer.\n");
+		return 1; /* Return an error code */
+	}
 
-    // Calculate and display the factorial
-    printf("The factorial of %d is: %llu\n", number, factorial(number));
+	/* Calculate and display the factorial */
+	printf("The factorial of %d is: %llu\n", number, calculate_factorial(number));
 
-    return 0;
+	return 0;
 }
 
